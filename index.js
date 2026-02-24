4db6e4641fa5a1569871c3c90746857a9cc60cf5
lisacainyc <237272486+lisacainyc@users.noreply.github.com>
2026-02-24 13:21:33 -0800

    Create 16x16 grid using JS

---
+++ index.js
@@ -0,0 +1,15 @@
+let grid = document.querySelector(".grid");
+
+function createGrid(n){
+    for (let i = 0; i < n; i++){
+        for (let j = 0; j < n; j++){
+            let unit = document.createElement("div");
+            unit.classList = "unit";
+            unit.textContent = `${i} ${j}`;
+            grid.appendChild(unit);
+        }
+
+    }
+}
+
+createGrid(16);
