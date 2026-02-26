f6b8910878c2b67f21eb101a2834b74caf507be2
lisacainyc <237272486+lisacainyc@users.noreply.github.com>
2026-02-26 12:56:14 -0800

    Add grid generator

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+let button = document.querySelector(".create");
 let grid = document.querySelector(".grid");
+let unit = document.querySelector(".unit");
+
+button.addEventListener('click', function(){
+    gridSize = Number(prompt("Enter a number between 1 and 100"));
+    createGrid(gridSize);
+
+})
 
 function createGrid(n){
+    grid.innerHTML = '';
     for (let i = 0; i < n; i++){
         for (let j = 0; j < n; j++){
             let unit = document.createElement("div");
             unit.classList = "unit";
-            unit.textContent = `${i} ${j}`;
+            unit.style.height = `calc(75vh / ${n})`;
+            unit.style.width = `calc(75vh / ${n})`;
             grid.appendChild(unit);
         }
-
+    
     }
-}
+};
 
-createGrid(16);
-
-grid.addEventListener('mouseover', (e) => e.target.style.backgroundColor = "black");
+grid.addEventListener('mouseover', function(e){
+    if (e.target.classList.contains("unit")){
+     e.target.style.backgroundColor = "black";}
+    });
